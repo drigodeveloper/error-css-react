04fa95780746cdb57b76cb148240f35583e74df8
Rodrigo <drigodeveloper@gmail.com>
2023-08-19 17:20:23 -0300

    Criando a pasta home

--- src/pages/Details/Index.jsx
+++ src/pages/Details/Index.jsx
@@ -1,15 +1,29 @@
-import { Container, Links } from "./style"
+import { Container, Links, Content } from "./style"
 import { Header } from "../../components/header"
 import { Button } from "../../components/button"
 import { Section } from "../../components/section"
 import { Tags } from "../../components/tags"
 import { ButtonText } from "../../components/ButtonText"
 
+
 export function Details() { 
   
   return(
     <Container>
       <Header />
+
+      <main>
+        <Content>
+
+          <h1>Introdução ao React</h1>
+          <p>React é uma biblioteca de JavaScript de código aberto e eficiente para a criação de interfaces de 
+            usuário. Desenvolvido pelo Facebook, o React permite criar componentes reutilizáveis que podem ser 
+            combinados para formar interfaces de usuário complexas.Uma das principais características do React é 
+            sua abordagem de renderização virtual. Em vez de atualizar a interface de usuário inteira quando 
+            ocorre uma alteração, o React atualiza apenas os componentes que foram afetados. Isso resulta em um 
+            desempenho excelente e uma experiência de usuário fluida.
+          </p>
+
 
       <Section title="Links úteis">
         <Links>
@@ -30,6 +44,8 @@ export function Details() {
 
       <ButtonText title="Excluir a nota" />
 
+        </Content>
+      </main>
       
 
     </Container>
--- src/pages/Details/style.js
+++ src/pages/Details/style.js
@@ -10,6 +10,12 @@ export const Container = styled.div`
     "header"
     "content";
 
+    > main {
+        grid-area: content;
+        overflow: scroll;
+        padding: 64px;
+    }
+
 `;
 
 export const Links = styled.ul `
@@ -23,8 +29,30 @@ export const Links = styled.ul `
 
     a {
         color: ${({theme}) => theme.COLORS.WHITE};
+        border: none;
     }
 
+`;
+
+export const Content = styled.div`
+    max-width: 550px;
+    margin: 0 auto;
+
+h1 {
+    margin-bottom: 16px;
+    color: var(--white, #F4EDE8);
+    font-family: Roboto Slab;
+    font-size: 36px;
+    font-style: normal;
+    }
+
+p {
+    color: ${({theme}) => theme.COLORS.WHITE};
+    text-align: justify;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: normal;    
+}
 `;
 
 
