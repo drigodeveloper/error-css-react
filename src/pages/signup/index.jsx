419f84ae2330efcfc8aba7c156de5c2f785ab710
Rodrigo Campos <drigodeveloper@gmail.com>
2023-08-23 14:09:07 -0300

    criando a pasta de signup

---
+++ src/pages/signup/index.jsx
@@ -0,0 +1,32 @@
+import { Container, Form, Background } from "./style";
+import { Input } from "../../components/Input"
+import { Button } from "../../components/button"
+
+export function Signin() {
+    return(
+    <Container>
+    <Form >
+
+       <h1>Rocket Notes</h1>
+       <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+       <h2>Faça seu login</h2>
+
+       <Input 
+       placeholder="E-mail"
+       />
+
+        <Input 
+       placeholder="Senha"
+       />
+
+        <Button title="Entrar" />
+
+        <a href="">Criar conta</a>
+       </Form>
+
+       <Background />
+    </Container>
+    )
+}
+
+
