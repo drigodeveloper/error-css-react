aed6d2b6c6f896619b8bb26e094e01160b294fbc
Rodrigo <drigodeveloper@gmail.com>
2023-10-21 20:16:01 -0300

    Criando a função de logout

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { api } from '../services/api'
 
 export const AuthContext = createContext({});
@@ -11,6 +11,9 @@ function AuthProvider({ children }) {
       try{
         const response = await api.post("/sessions", {email, password})
           const { user, token }  = response.data;
+
+          localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
+          localStorage.setItem("@rocketnotes:token", token)
 
           api.defaults.headers.authorizations = `Bearer ${token}`
           setData({user, token})
@@ -26,7 +29,26 @@ function AuthProvider({ children }) {
 
     }
 
+    async function signOut() {
+      const token = localStorage.removeItem('@rocketnotes:token');
+      const user = localStorage.removeItem('@rocketnotes:user');
 
+      setData({});
+    }
+
+    useEffect(() => {
+      const token = localStorage.getItem('@rocketnotes:token');
+      const user = localStorage.getItem('@rocketnotes:user');
+
+      if(token && user) {
+        api.defaults.headers.authorizations = `Bearer ${token}`
+        
+        setData({
+          token,
+          user: JSON.parse(user)
+        })
+      }
+    }, []);
 
 
     return (
