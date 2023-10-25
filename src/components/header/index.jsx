e815529ae1822d4f5ccf97d66f9a8ccf022e18c8
Rodrigo <drigodeveloper@gmail.com>
2023-10-24 22:15:04 -0300

    Criando a função de Upload de imagem

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,19 +1,25 @@
 import { useAuth } from '../../hooks/auth'
 import { RiShutDownLine } from "react-icons/ri";
+import { api } from "../../services/api"
+import AvatarPlaceHolder from "../../assets/avatar_placeholder.svg" 
+
+
 
 
 import { Container, Profile, Logout } from "./styles";
 
 export function Header() {
-    const { signOut } = useAuth();  
+    const { signOut, user } = useAuth();  
+    const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : AvatarPlaceHolder;
+
     return (
     <Container>
         <Profile to="/profile">
-        <img src="https://github.com/drigodeveloper.png" alt="imagem de perfil do usuario" />
+        <img src={avatarUrl} alt={user.name} />
 
         <div>
             <span>Bem-vindo</span>
-            <strong>Rodrigo Campos</strong>
+            <strong>{user.name}</strong>
         </div>  
         </Profile>
 
--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,11 +1,49 @@
+import { useState } from "react";
+import { useAuth } from '../../hooks/auth'
 import { Container, Form, Avatar } from "./style";
 import { FiArrowLeft } from "react-icons/fi"
 import { FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi"
 import { Input } from "../../components/Input"
 import { Button } from "../../components/button"
 import { Link } from "react-router-dom"
+import { api } from "../../services/api"
+import AvatarPlaceHolder from "../../assets/avatar_placeholder.svg" 
 
 export function Profile({icon: Icon}) {
+
+    const { user, updateProfile } = useAuth();
+    const [name, setName] = useState(user.name);
+    const [email, setEmail] = useState(user.email);
+    const [oldPassword, setOldPassword] = useState();
+    const [newPassword, setNewPassword] = useState();
+    const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : AvatarPlaceHolder;
+    const [avatar, setAvatar] = useState(avatarUrl);
+    const [avatarFile, setAvatarFile] = useState(null);
+
+    async function handleUpdate() {
+        const user = {
+            name,
+            email,
+            password: newPassword,
+            old_password: oldPassword
+        }
+        await updateProfile({ user, avatarFile })
+    }
+
+    async function handleChangeAvatar(event) {
+        const file = event.target.files[0];
+
+        setAvatarFile(file);
+
+        const imagePreview = URL.createObjectURL(file);
+
+        setAvatar(imagePreview);
+
+    }
+
+    
+
+
     return (
         <Container>
             <header>
@@ -16,36 +54,46 @@ export function Profile({icon: Icon}) {
 
             <Form action="">
                 <Avatar >
-                    <img src="https://github.com/drigodeveloper.png" alt="Foto do usuario" />
+                    <img src={avatar} />
                     <label htmlFor="avatar">
                     <FiCamera />
-                    <input id="avatar" type="file"/>
+                    <input id="avatar" type="file" onChange={handleChangeAvatar}/>
                     </label>
 
                 </Avatar>
                 <Input 
                 placeholder="Nome"
                     icon={FiUser}
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+
                 />
 
                 <Input 
                     placeholder="E-mail"
                     icon={FiMail}
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+
                 />
 
                 <Input 
                     placeholder="Senha atual"
                     type="password"
                     icon={FiLock}
+                    onChange={e => setOldPassword(e.target.value)}
+                    
                 />
 
                 <Input 
                     placeholder="Nova senha"
                     type="password"
                     icon={FiLock}
+                    onChange={e => setNewPassword(e.target.value)}
+
                 />
 
-                <Button title="Salvar" />
+                <Button title="Salvar" onClick={handleUpdate}/>
             </Form>
         </Container>
     )
